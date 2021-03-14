15eabebbc5c08fbc976080d173027b3744cb9589
Javier Eche <javier.eche1205@gmail.com>
2021-03-14 16:54:53 -0500

    configured serializer to show details of product into order

--- order/serializers.py
+++ order/serializers.py
@@ -1,7 +1,17 @@
 from .models import Order
+from product.models import Product
 from rest_framework import serializers
 
+
+class ProductItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name', 'image', 'type_product', 'price')
+
+
 class OrderSerializer(serializers.ModelSerializer):
+    products = ProductItemSerializer(many=True, read_only=True, required=False)
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ('name_client', 'status', 'products', 'amount')
